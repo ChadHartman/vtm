5f737038377931b5e02f1f6cd3ea8ae0df800ca4
chadhartman <chad+github@tealium.com>
2020-04-01 11:52:23 -0700

    Adding filter prep

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,7 @@ let app = {
     state: {
         characters: [],
         relationships: [],
+        tags: new Set(),
         templates: {},
         req_attempt: 0,
         req_success: 0
@@ -50,6 +51,28 @@ let app = {
                 mapContainer.find(".char-info").remove();
             },
 
+            addRelationships: () => {
+
+                const offsetX = 50;
+                const offsetY = 50;
+                let canvas = $("#rel-map canvas").get(0);
+                let ctx = canvas.getContext("2d");
+                ctx.lineWidth = 5;
+                ctx.strokeStyle = "white";
+
+                ctx.beginPath();
+
+                for (let rel of app.state.relationships) {
+                    let startChar = app.util.findChar(rel.characters[0]);
+                    let stopChar = app.util.findChar(rel.characters[1]);
+
+                    ctx.moveTo(startChar.loc.x + offsetX, startChar.loc.y + offsetY);
+                    ctx.lineTo(stopChar.loc.x + offsetX, stopChar.loc.y + offsetY);
+                }
+
+                ctx.stroke();
+            },
+
             addCharacters: () => {
                 let mapContainer = $("#rel-map");
                 let canvas = mapContainer.find("canvas").get(0);
@@ -64,13 +87,18 @@ let app = {
                 for (let i = 0; i < app.state.characters.length; i++) {
                     let character = app.state.characters[i];
                     let a = i * aStep;
-                    let x = cx + r * Math.cos(a)
-                    let y = cy + r * Math.sin(a)
+                    let x = Math.round(cx + r * Math.cos(a) - 64);
+                    let y = Math.round(cy + r * Math.sin(a) - 32);
+
+                    character.loc = {
+                        x: x,
+                        y: y
+                    };
 
                     mapContainer.append(
                         $(Mustache.render(charInfoTemplate, character))
-                        .css("left", Math.round(x) - 64)
-                        .css("top", Math.round(y) - 32)
+                        .css("left", x)
+                        .css("top", y)
                         .on("click", {
                                 "id": character.id
                             },
@@ -92,6 +120,7 @@ let app = {
                 }
 
                 app.ui.relMap.addCharacters();
+                app.ui.relMap.addRelationships();
             }
 
         }
@@ -121,20 +150,37 @@ let app = {
     character: {
 
         onLoad: (json, status, jqXHR) => {
+
             let id = jqXHR.responseURL.substring(
                 jqXHR.responseURL.lastIndexOf("/") + 1,
                 jqXHR.responseURL.lastIndexOf("."));
+
             ++app.state.req_success;
 
-            if ("characters" === id) {
-                app.state.characters = json;
-                app.ui.relMap.populate();
-            } else {
-                let character = app.util.findChar(id);
-                character.detail = json;
-                $("#modal").html(Mustache.render(
-                    app.state.templates["char-detail"],
-                    character));
+            switch (id) {
+
+                case "characters":
+                    for (let character of json) {
+                        for (let tag of character.tags) {
+                            app.state.tags.add(tag);
+                        }
+                    }
+                    app.state.characters = json;
+                    app.ui.relMap.populate();
+                    break;
+
+                case "relationships":
+                    app.state.relationships = json;
+                    app.ui.relMap.populate();
+                    break;
+
+                default:
+                    let character = app.util.findChar(id);
+                    character.detail = json;
+                    $("#modal").html(Mustache.render(
+                        app.state.templates["char-detail"],
+                        character));
+                    break;
             }
         },
 
@@ -154,6 +200,7 @@ let app = {
         app.template.load("char-detail");
         app.template.load("char-info");
         app.character.load("characters");
+        app.character.load("relationships");
 
         window.onresize = app.ui.relMap.populate;
         $("#overlay").hide().click(() => $("#overlay").hide());
