dc92c47865c5ac81b9a20034d10b8487e6db1e04
chadhartman <chad+github@tealium.com>
2020-04-01 10:28:03 -0700

    Restructure

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,132 +1,147 @@
-let app = {};
+let app = {
 
-((app) => {
-
-    app.state = {
+    state: {
         characters: [],
+        relationships: [],
         templates: {},
         req_attempt: 0,
         req_success: 0
-    };
+    },
 
-    app.findChar = (id) => {
-        for (let character of app.state.characters) {
-            if (character.id == id) {
-                return character;
+    util: {
+        findChar: (id) => {
+            for (let character of app.state.characters) {
+                if (character.id == id) {
+                    return character;
+                }
+            }
+        },
+
+        isFetchingResources: () => {
+            return req_attempt !== req_success;
+        }
+    },
+
+    ui: {
+        onCharClick: (e) => {
+            $("#overlay").show();
+
+            let character = app.util.findChar(e.data.id);
+            if (character.detail) {
+                $("#modal").html(Mustache.render(
+                    app.state.templates["char-detail"],
+                    character));
+            } else {
+                app.character.load(e.data.id);
+            }
+        },
+
+        loadRelMap: () => {
+
+            // Resize
+            let mapContainer = $("#rel-map");
+            let canvas = mapContainer.find("canvas").get(0);
+            canvas.width = mapContainer.width();
+            canvas.height = mapContainer.height();
+
+            if (app.state.characters.length === 0) {
+                return;
+            }
+
+            // Cleanup
+            mapContainer.find(".char-info").remove();
+
+            if (app.state.req_attempt !== app.state.req_success) {
+                // Content is still loading
+                return;
+            }
+
+            // Add divs
+            let cx = Math.round(canvas.width / 2.0);
+            let cy = Math.round(canvas.height / 2.0);
+            let aStep = Math.PI * 2 / app.state.characters.length;
+            let r = (cx < cy ? cx : cy) / 1.3;
+            let charInfoTemplate = app.state.templates["char-info"];
+
+            for (let i = 0; i < app.state.characters.length; i++) {
+                let character = app.state.characters[i];
+                let a = i * aStep;
+                let x = cx + r * Math.cos(a)
+                let y = cy + r * Math.sin(a)
+
+                mapContainer.append(
+                    $(Mustache.render(charInfoTemplate, character))
+                    .css("left", Math.round(x) - 64)
+                    .css("top", Math.round(y) - 32)
+                    .on("click", {
+                            "id": character.id
+                        },
+                        app.ui.onCharClick));
             }
         }
-    }
+    },
 
-    let onCharClick = (e) => {
-        $("#overlay").show();
+    template: {
+        onLoad: (html, status, jqXHR) => {
+            let id = jqXHR.responseURL.substring(
+                jqXHR.responseURL.lastIndexOf("/") + 1,
+                jqXHR.responseURL.lastIndexOf("."));
 
-        let character = app.findChar(e.data.id);
-        if (character.detail) {
-            $("#modal").html(Mustache.render(
-                app.state.templates["char-detail"],
-                character));
-        } else {
-            app.loadData(e.data.id);
+            app.state.templates[id] = html;
+            ++app.state.req_success;
+            app.ui.loadRelMap();
+        },
+
+        load: (name) => {
+            ++app.state.req_attempt;
+            $.ajax({
+                url: `assets/template/${name}.html`,
+                success: app.template.onLoad,
+                dataType: "html"
+            });
         }
-    };
+    },
 
-    let loadRelMap = () => {
+    character: {
 
-        // Resize
-        let mapContainer = $("#rel-map");
-        let canvas = mapContainer.find("canvas").get(0);
-        canvas.width = mapContainer.width();
-        canvas.height = mapContainer.height();
+        onLoad: (json, status, jqXHR) => {
+            let id = jqXHR.responseURL.substring(
+                jqXHR.responseURL.lastIndexOf("/") + 1,
+                jqXHR.responseURL.lastIndexOf("."));
+            ++app.state.req_success;
 
-        if (app.state.characters.length === 0) {
-            return;
+            if ("characters" === id) {
+                app.state.characters = json;
+                app.ui.loadRelMap();
+            } else {
+                let character = app.util.findChar(id);
+                character.detail = json;
+                $("#modal").html(Mustache.render(
+                    app.state.templates["char-detail"],
+                    character));
+            }
+        },
+
+
+        load: (name) => {
+            ++app.state.req_attempt;
+            $.ajax({
+                url: `assets/data/${name}.json`,
+                success: app.character.onLoad,
+                dataType: "json"
+            });
         }
 
-        // Cleanup
-        mapContainer.find(".char-info").remove();
+    },
 
-        if (app.state.req_attempt !== app.state.req_success) {
-            // Content is still loading
-            return;
-        }
+    start: () => {
+        app.template.load("char-detail");
+        app.template.load("char-info");
+        app.character.load("characters");
 
-        // Add divs
-        let cx = Math.round(canvas.width / 2.0);
-        let cy = Math.round(canvas.height / 2.0);
-        let aStep = Math.PI * 2 / app.state.characters.length;
-        let r = (cx < cy ? cx : cy) / 1.3;
-        let charInfoTemplate = app.state.templates["char-info"];
-
-        for (let i = 0; i < app.state.characters.length; i++) {
-            let character = app.state.characters[i];
-            let a = i * aStep;
-            let x = cx + r * Math.cos(a)
-            let y = cy + r * Math.sin(a)
-
-            mapContainer.append(
-                $(Mustache.render(charInfoTemplate, character))
-                .css("left", Math.round(x) - 64)
-                .css("top", Math.round(y) - 32)
-                .on("click", {
-                        "id": character.id
-                    },
-                    onCharClick));
-        }
-    };
-
-    let onTemplateLoaded = (html, status, jqXHR) => {
-        let id = jqXHR.responseURL.substring(
-            jqXHR.responseURL.lastIndexOf("/") + 1,
-            jqXHR.responseURL.lastIndexOf("."));
-
-        app.state.templates[id] = html;
-        ++app.state.req_success;
-        loadRelMap();
-    };
-
-    let loadTemplate = (name) => {
-        ++app.state.req_attempt;
-        $.ajax({
-            url: `assets/template/${name}.html`,
-            success: onTemplateLoaded,
-            dataType: "html"
-        });
-    };
-
-    let onDataLoaded = (json, status, jqXHR) => {
-        let id = jqXHR.responseURL.substring(
-            jqXHR.responseURL.lastIndexOf("/") + 1,
-            jqXHR.responseURL.lastIndexOf("."));
-        ++app.state.req_success;
-
-        if ("characters" === id) {
-            app.state.characters = json;
-            loadRelMap();
-        } else {
-            let character = app.findChar(id);
-            character.detail = json;
-            $("#modal").html(Mustache.render(
-                app.state.templates["char-detail"],
-                character));
-        }
-    };
-
-    app.loadData = (name) => {
-        ++app.state.req_attempt;
-        $.ajax({
-            url: `assets/data/${name}.json`,
-            success: onDataLoaded,
-            dataType: "json"
-        });
-    };
-
-    $(document).ready(() => {
-
-        loadTemplate("char-detail");
-        loadTemplate("char-info");
-        app.loadData("characters");
-
-        window.onresize = loadRelMap;
+        window.onresize = app.ui.loadRelMap;
         $("#overlay").hide().click(() => $("#overlay").hide());
-    });
-})(app);
+    }
+};
+
+$(document).ready(app.start);
